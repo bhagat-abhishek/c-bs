d7fa14681de023fcf29279d1544abb09f1c4067a
Abishek Bhagat <85464874+bhagat-abhishek@users.noreply.github.com>
2024-08-10 18:41:51 +0000

    2

---
+++ outs/2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+     *
+     * Print the sum of first (n) numbers given by a user
+     *   e.g. (3) User input
+     *   output = 6 [1+2+3]
+     *
+     */
+
+    int n;
+    int out = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        out = out + i;
+    }
+
+    printf("Sum of number %d is %d\n", n, out);
+}
